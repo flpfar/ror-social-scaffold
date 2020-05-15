459728a3569e6f4e00bef5701b962115a4142d70
sergiomauz <sergio.mauz88@gmail.com>
2020-05-15 15:05:06 -0500

    Added friendship testing

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,23 +7,40 @@ RSpec.describe 'User', type: :feature do
     User.create(name: 'Selena Perez', email: 'selena@mail.com', password: '123456')
     User.create(name: 'Elvis Presley', email: 'elvis@mail.com', password: '123456')
     User.first.friendships.create(friend_id: 3, accepted: true)
+    User.last.friendships.create(friend_id: 1, accepted: false)
   end
 
-  context 'when logged in and in -all users- page' do
+  context 'when logged in as Sergio' do
     before(:each) do
       visit new_user_session_path
       fill_in 'user_email', with: 'sergiomauz@mail.com'
       fill_in 'user_password', with: '123456'
       find("input[type='submit']").click
-      visit users_path
+      
     end
-    it 'sends an invitation to a new friend' do
-      find('li', text: 'Felipe').click_link('Send an invitation')
-      expect(page).to have_content('Invitation sent')
-    end
-    it 'removes a friend' do
-      find('li', text: 'Selena').click_link('Remove friend')
-      expect(page).to have_content('Friendship removed')
+
+    context 'when you are in -all users- page' do
+      before(:each) do
+        visit users_path
+      end
+
+      it 'sends an invitation to a new friend' do
+        find('li', text: 'Felipe').click_link('Send an invitation')
+        expect(page).to have_content('Invitation sent')
+      end
+
+      it 'removes a friend' do
+        find('li', text: 'Selena').click_link('Remove friend')
+        expect(page).to have_content('Friendship removed')
+      end       
+    end  
+    
+    context 'when you are in -friends- page' do
+      it 'accepts a friendship invitation' do
+        visit friendships_path
+        click_link('(Accept friendship request)')
+        expect(page).to have_content('Friendship accepted!')
+      end      
     end
   end
 
