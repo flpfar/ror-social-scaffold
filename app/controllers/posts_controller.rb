a949b38365c63999e66d06b70590f2ba21fdc614
Felipe Rosa <flp.far@gmail.com>
2020-05-18 19:23:14 -0300

    Add posts from friends in timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,9 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    ids = current_user.active_friends.pluck(:id)
+    ids << current_user.id
+    @timeline_posts ||= Post.where(user_id: ids).order('created_at DESC')
   end
 
   def post_params
