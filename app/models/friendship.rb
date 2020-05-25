a4e3127f0e68c23176facbf819736678447f71ba
Felipe Rosa <flp.far@gmail.com>
2020-05-25 11:53:14 -0300

    Add validation to frienship uniqueness

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   validate :not_self_friendship
+  validates :user_id, uniqueness: { scope: :friend_id }
 
   def not_self_friendship
     errors.add(:friend_id, 'User can not send a self friendship request.') if user_id == friend_id
